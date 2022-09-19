68cfa30908051a369d54e8fd3f0ea2e59207b1f2
AkhilVarma-dev <108959067+AkhilVarma-dev@users.noreply.github.com>
2022-09-19 21:56:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Index
+{
+    public static void Main()
+    {
+        int[] arr = new int[5] { 10, 20, 30, 50, 40 };
+
+        Array.Sort(arr);
+        foreach (int i in arr)
+        {
+            Console.Write(i + ",");
+        }
+        
+        Console.WriteLine();
+        Console.WriteLine("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+        
+        for ( int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] < num)
+            {
+                i++;
+                if (arr[i] > num)
+                {
+                    Console.WriteLine("index value of given number is :{0}", i);
+                }                
+                
+            }
+            
+        }
+
+    }
+
+}
